eb7b0107e49c3cb009c15c8f4cc39ffc2ea45536
rickv-max <rickymaulana080503@gmail.com>
2025-07-21 07:32:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// Toggle hamburger menu
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
 
   hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+  });
+
+  // Sticky Navbar saat scroll
+  const navbar = document.querySelector(".navbar");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
   });
 });
