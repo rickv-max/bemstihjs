ca5d589b77054d486673b77f9039d1a49a5283ff
rickv-max <rickymaulana080503@gmail.com>
2025-07-21 07:24:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function toggleMenu() {
-  document.getElementById('navLinks').classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
-function openForm() {
-  document.getElementById('formModal').style.display = 'block';
-}
-function closeForm() {
-  document.getElementById('formModal').style.display = 'none';
-}
-window.onclick = function (event) {
-  const modal = document.getElementById('formModal');
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
